e1d9d1d573b2b648ca104c25b66f26174ac982bd
nithyamn <nithya.mn999@gmail.com>
2020-03-30 16:30:37 +0530

    Update LocalTest.java

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -7,8 +7,8 @@ public class LocalTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        //driver.get("http://bs-local.com:45691/check");
-        driver.get("http://localhost:8000/");
-        Assert.assertTrue(driver.getTitle().contains("Demo Site"));
+        driver.get("http://bs-local.com:45691/check");
+        //driver.get("http://localhost:8000/");
+        //Assert.assertTrue(driver.getTitle().contains("Demo Site"));
     }
 }
