23bd9aa58f28d8ecd0177eff8e3d33bd2c0be9ce
nithyamn <nithya.mn999@gmail.com>
2020-09-07 17:46:04 +0530

    Create TestStatus.java

---
+++ src/test/java/com/browserstack/TestStatus.java
@@ -0,0 +1,47 @@
+package com.browserstack;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.remote.SessionId;
+
+import javax.mail.MessagingException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class TestStatus {
+    public static void mark(SessionId sessionId, String status, String username, String accesskey) throws Exception {
+        URI uri = new URI("https://"+username+":"+accesskey+"@api.browserstack.com/automate/sessions/"+sessionId+".json");
+        HttpPut putRequest = new HttpPut(uri);
+
+
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add((new BasicNameValuePair("status", status)));
+        nameValuePairs.add((new BasicNameValuePair("reason", "")));
+
+        putRequest.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+        HttpClientBuilder.create().build().execute(putRequest);
+
+    }
+}
