63204221c2987ad3b12b9ac077c30eb9a2577564
nithyamn <nithya.mn999@gmail.com>
2020-09-07 17:45:19 +0530

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -10,12 +10,20 @@ public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
+        String username = BrowserStackTestNGTest.username;
+        String accesskey = BrowserStackTestNGTest.accessKey;
+
+        
         driver.get("https://www.google.com/ncr");
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("BrowserStack");
         element.submit();
-        //Thread.sleep(100000);
+        SessionId session = ((RemoteWebDriver) driver).getSessionId();
+        System.out.println(session);
 
-        Assert.assertEquals("BrowserStack - Google Search", driver.getTitle());
+        if(title.contains("BrowserStack"))
+            TestStatus.mark(session,"passed", username, accesskey);
+        else
+            TestStatus.mark(session,"failed", username, accesskey);
     }
 }
