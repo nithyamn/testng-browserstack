8475e0247841d79f505fb8aa439657fcf03239c2
nithyamn <nithya.mn999@gmail.com>
2020-10-13 17:14:05 +0530

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.SessionId;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class SingleTest extends BrowserStackTestNGTest {
 
@@ -24,13 +25,17 @@ public class SingleTest extends BrowserStackTestNGTest {
         String title = driver.getTitle();
         SessionId session = ((RemoteWebDriver) driver).getSessionId();
         System.out.println(session);
+        
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"org.openqa.selenium.TimeoutException: Expected condition failed: waiting for title to be Hotel Search Results. Current title: (tried for 45 second(s) with 500 milliseconds interval) Build info: version: '3.141.59', revision: 'e82be7d358', time: '2018-11\"}}");
 
-        if(title.equals("BrowserStack - Google Search")) {
+
+        /*if(title.equals("BrowserStack - Google Search")) {
             TestStatus.mark(session, "passed", username, accesskey);
         }
         else{
             TestStatus.mark(session,"failed", username, accesskey);
             Assert.assertEquals("BrowserStack - Google Search", driver.getTitle());
-        }
+        }*/
     }
 }
