892ced52a29c1b8e06da1c4d0e043592a0a5c6fc
nithyamn <nithya.mn999@gmail.com>
2020-10-14 19:18:03 +0530

    Update LocalTest.java

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -2,6 +2,7 @@ package com.browserstack;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class LocalTest extends BrowserStackTestNGTest {
 
@@ -10,5 +11,7 @@ public class LocalTest extends BrowserStackTestNGTest {
         driver.get("http://bs-local.com:45691/check");
         //driver.get("http://localhost:8000/");
         //Assert.assertTrue(driver.getTitle().contains("Demo Site"));
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Validated\"}}");
     }
 }
