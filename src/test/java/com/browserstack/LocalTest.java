4006f0ddfd526722d23f3602db16a2b0826b17d8
nithyamn <nithya.mn999@gmail.com>
2020-11-03 20:23:39 +0530

    Update LocalTest.java

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -9,14 +9,15 @@ public class LocalTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        driver.get("http://localhost:45691");
+        driver.get("http://localhost:45454");
+        //driver.get("http://localhost:45691");
         //driver.get("http://localhost/bs_test/callback.php");
         //driver.get("http://localhost:8000/");
         //Assert.assertTrue(driver.getTitle().contains("Demo Site"));
         JavascriptExecutor jse = (JavascriptExecutor)driver;
-        if(driver.findElement(By.xpath("/html/body")).getText().equals("This is an internal server for BrowserStack Local"))
+        /*if(driver.findElement(By.xpath("/html/body")).getText().equals("This is an internal server for BrowserStack Local"))
            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Validated\"}}");
        else
-           jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"Not Validated\"}}");
+           jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"Not Validated\"}}");*/
     }
 }
